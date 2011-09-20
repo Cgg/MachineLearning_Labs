9296a61c49130cb49d142a9ef49cb2535ecba57c
Cgg <clement.geiger@gmail.com>
2011-09-20 17:53:07 +0200

    +a4

--- Lab1/assignments.py
+++ Lab1/assignments.py
@@ -43,6 +43,56 @@ def A31():
   print tree
   draw.drawTree( tree )
 
+
+# ----------------------------------------------
+def A4( number ):
+  fractions = [ 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9 ]
+
+  results = [[0 for col in range(2)] for row in range(len(fractions))]
+
+  intermediatesResults = [0 for i in range(number)]
+
+# M1
+  for f in fractions:
+    for i in range( number ):
+      intermediatesResults[ i ] = pruning( m.monk1, m.monk1test, f )
+
+    results[ fractions.index(f) ][ 0 ] = sum(intermediatesResults)/len(intermediatesResults)
+
+#M3
+  for f in fractions:
+    for i in range( number ):
+      intermediatesResults[ i ] = pruning( m.monk3, m.monk3test, f )
+
+    results[ fractions.index(f) ][ 1 ] = sum(intermediatesResults)/len(intermediatesResults)
+
+  for i in range(len(fractions)):
+    print fractions[ i ], '\t', results[i][0], '\t', results[i][1]
+
+
+# --------------------------------------------
+def pruning( trainingSet, testSet, fraction ):
+  train1, train2 = partition( trainingSet, fraction )
+
+  bestTree = dT.buildTree( train1, m.attributes )
+  bestTreePerf = dT.check( bestTree, train2 )
+  bestTreeFound = True
+
+  while bestTreeFound == True:
+    bestTreeFound = False
+
+    prunedTrees = dT.allPruned( bestTree )
+
+    for candidateTree in prunedTrees:
+
+      if dT.check( candidateTree, train2 ) >= bestTreePerf:
+        bestTree = candidateTree
+        bestTreePerf = dT.check( candidateTree, train2 )
+        bestTreeFound = True
+
+  return dT.check( bestTree, testSet )
+
+
 def partition( data, fraction ):
   ldata = list( data )
   random.shuffle( ldata )
